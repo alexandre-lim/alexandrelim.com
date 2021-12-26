3b3bbc0eaee09b4fe303aa73e9d309085791f43b
Alexandre Lim <alexandre.lim94@gmail.com>
2021-12-26 16:15:42 +0100

    feat(homepage): add Document and Layout components to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+import { Link, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
 import type { MetaFunction, LinksFunction } from 'remix';
 
 import tailwindStyles from './styles/tailwind.css';
@@ -25,19 +25,64 @@ export const links: LinksFunction = () => {
 
 export default function App() {
   return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }: { children: React.ReactNode; title?: string }) {
+  return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div id="root" className="flex flex-col min-h-full">
+      <header className="mt-12 w-full max-w-5xl mx-auto px-8 flex items-center justify-between h-16">
+        <Link to="/" title="Home">
+          <h1>Alexandre Lim</h1>
+        </Link>
+        <nav aria-label="Main navigation">
+          <ul className="flex gap-8">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/experience">Experience</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/growth">Personal Growth</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer className="mt-auto p-4 border border-solid border-zinc-300">
+        <div className="text-center">
+          <p className="text-xs">© 2022-present Alexandre Lim. All Rights Reserved.</p>
+        </div>
+      </footer>
+    </div>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,3 @@
 export default function Index() {
-  return (
-    <div id="root">
-      <h1>Alexandre Lim</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
