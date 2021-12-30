c867cbf5df9757800fd248147f7ace40f7e6e7a6
Alexandre Lim <alexandre.lim94@gmail.com>
2021-12-30 17:39:42 +0100

    feat(about): add layout skeleton

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,7 +1,93 @@
+import { TriangleRightIcon } from '@radix-ui/react-icons';
+
 export default function Index() {
   return (
     <>
-      <div>About page</div>
+      <div className="flex flex-col gap-16 max-w-2xl mx-auto">
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </section>
+
+        <section>
+          <div className="mb-8">
+            <h2 className="text-xl md:text-2xl font-bold">Education</h2>
+          </div>
+          <ul className="pl-4 flex flex-col gap-2">
+            <li>
+              <div className="flex gap-2">
+                <TriangleRightIcon className="text-gray-800 text-opacity-80 min-w-[14px]" height={24} width={24} />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+              </div>
+            </li>
+            <li>
+              <div className="flex gap-2">
+                <TriangleRightIcon className="text-gray-800 text-opacity-80 min-w-[14px]" height={24} width={24} />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <div className="mb-8">
+            <h2 className="text-xl md:text-2xl font-bold">Influence&nbsp;of&nbsp;sports</h2>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </section>
+
+        <section>
+          <div className="mb-8">
+            <h2 className="text-xl md:text-2xl font-bold">Values</h2>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </section>
+
+        <section>
+          <div className="mb-8">
+            <h2 className="text-xl md:text-2xl font-bold">Anti-fragile&nbsp;identity</h2>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </section>
+
+        <section>
+          <div className="mb-8">
+            <h2 className="text-xl md:text-2xl font-bold">Hobbies&nbsp;&&nbsp;Interests</h2>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </section>
+      </div>
     </>
   );
 }
