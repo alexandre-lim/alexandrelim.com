6bc8e42cc8de6d134193f4bd7e84d79fb441d3fd
Alexandre Lim <alexandre.lim94@gmail.com>
2022-01-11 22:11:27 +0100

    feat(link): add LinkArrowRight component

---
+++ app/components/LinkArrowRight.tsx
@@ -0,0 +1,23 @@
+import { Link, LinkProps } from 'remix';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
+
+interface LinkArrowRightProps extends LinkProps {
+  text: string;
+}
+
+function LinkArrowRight({ text, to }: LinkArrowRightProps) {
+  return (
+    <Link to={to} className="group flex gap-2 hover:text-blue-500 font-recursive-semi-casual font-recursive-medium">
+      <span className="transition-[font-variation-settings] duration-300 group-hover:font-recursive-slant-max">
+        {text}
+      </span>
+      <ArrowRightIcon
+        height={24}
+        width={24}
+        className="transition-transform duration-500 group-hover:translate-x-0.5"
+      />
+    </Link>
+  );
+}
+
+export { LinkArrowRight };
