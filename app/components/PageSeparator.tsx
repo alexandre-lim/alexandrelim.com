34cdf191b8f69ff8c636bf0bc135891cc6ffe170
Alexandre Lim <alexandre.lim94@gmail.com>
2022-01-15 14:11:45 +0100

    feat(page-separator): add PageSeparator component in blog, books and courses review index route

---
+++ app/components/PageSeparator.tsx
@@ -0,0 +1,7 @@
+import * as Separator from '@radix-ui/react-separator';
+
+function PageSeparator() {
+  return <Separator.Root orientation="horizontal" className="h-px w-4/5 bg-gray-300 mb-12 mx-auto" />;
+}
+
+export { PageSeparator };
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,65 +1,69 @@
 import { Link } from 'remix';
 
 import { ROUTES } from '~/routes';
+import { PageSeparator } from '~/components/PageSeparator';
 
 export default function Index() {
   return (
     <>
-      <div className="flex flex-col gap-8">
-        <div>
-          <h2 className="text-xl md:text-2xl font-bold">Blog</h2>
-          <p className="mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore ma
-          </p>
-        </div>
-        <div className="grid grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))] gap-8">
-          <article className="border rounded-lg shadow-md">
-            <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
-              <h3>Article title</h3>
-              <div className="my-4">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat.
-                </p>
-              </div>
-              <div>Read More</div>
-            </Link>
-          </article>
+      <div className="max-w-2xl mx-auto mb-12">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </div>
 
-          <article className="border rounded-lg shadow-md">
-            <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
-              <h3>Article title</h3>
-              <div className="my-4">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-              <div>Read More</div>
-            </Link>
-          </article>
+      <PageSeparator />
 
-          <article className="border rounded-lg shadow-md">
-            <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
-              <h3>Article title</h3>
-              <div className="my-4">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-              <div>Read More</div>
-            </Link>
-          </article>
-        </div>
+      <div className="grid grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))] gap-8">
+        <article className="border rounded-lg shadow-md">
+          <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
+            <h3>Article title</h3>
+            <div className="my-4">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat.
+              </p>
+            </div>
+            <div>Read More</div>
+          </Link>
+        </article>
+
+        <article className="border rounded-lg shadow-md">
+          <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
+            <h3>Article title</h3>
+            <div className="my-4">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div>Read More</div>
+          </Link>
+        </article>
+
+        <article className="border rounded-lg shadow-md">
+          <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
+            <h3>Article title</h3>
+            <div className="my-4">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </div>
+            <div>Read More</div>
+          </Link>
+        </article>
       </div>
     </>
   );
