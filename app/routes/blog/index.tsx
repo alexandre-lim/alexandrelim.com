a7ad2044f7503ccdc7f16dea81b7e20f718b4ab1
Alexandre Lim <alexandre.lim94@gmail.com>
2022-01-15 14:46:29 +0100

    feat(blog): add more customization to ArticleCard and use the component in blog index route

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,7 +1,6 @@
-import { Link } from 'remix';
-
 import { ROUTES } from '~/routes';
 import { PageSeparator } from '~/components/PageSeparator';
+import { ArticleCard, ArticleHeaderTag } from '~/components/ArticleCard';
 
 export default function Index() {
   return (
@@ -19,51 +18,41 @@ export default function Index() {
       <PageSeparator />
 
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))] gap-8">
-        <article className="border rounded-lg shadow-md">
-          <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
-            <h3>Article title</h3>
-            <div className="my-4">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
-              </p>
-            </div>
-            <div>Read More</div>
-          </Link>
-        </article>
+        <ArticleCard
+          to={`${ROUTES.blog}/article-example`}
+          headerTag={ArticleHeaderTag.h2}
+          title="Article Title"
+          arrowText="Read More"
+          summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum."
+        />
 
-        <article className="border rounded-lg shadow-md">
-          <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
-            <h3>Article title</h3>
-            <div className="my-4">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-              </p>
-            </div>
-            <div>Read More</div>
-          </Link>
-        </article>
+        <ArticleCard
+          to={`${ROUTES.blog}/article-example`}
+          headerTag={ArticleHeaderTag.h2}
+          title="Article Title"
+          arrowText="Read More"
+          summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum."
+        />
 
-        <article className="border rounded-lg shadow-md">
-          <Link to={`${ROUTES.blog}/article-example`} className="block h-full p-8">
-            <h3>Article title</h3>
-            <div className="my-4">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-              </p>
-            </div>
-            <div>Read More</div>
-          </Link>
-        </article>
+        <ArticleCard
+          to={`${ROUTES.blog}/article-example`}
+          headerTag={ArticleHeaderTag.h2}
+          title="Article Title"
+          arrowText="Read More"
+          summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum."
+        />
       </div>
     </>
   );
